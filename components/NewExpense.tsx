fb2048ca70c9a7909ac5c77e35df7fa8c3a2d1f6
Christian Alejandro <christian.aguero.1983@gmail.com>
2023-02-23 00:06:03 +0100

    field name changes

--- components/NewExpense.tsx
+++ components/NewExpense.tsx
@@ -22,10 +22,10 @@ interface TransactionInfo {
 
 const formatDate = (date: string) => {
   //const dateString = '20/02/2023';
-const [day, month, year] = date.split('/');
+  const [day, month, year] = date.split("/");
   const dateObject = new Date(`${year}-${month}-${day}`);
-  return dateObject
-}
+  return dateObject;
+};
 
 function parseTransactionInfo(text: string): any | null {
   const fields = text.split("\n").map((line) => line.trim());
@@ -33,36 +33,42 @@ function parseTransactionInfo(text: string): any | null {
   const matchType = fields[0].match(/^Tipo de movimiento\s+(.+)/);
   const type = matchType ? matchType[1] : "";
 
-  const matchDescription = fields[1] && fields[1].match(/^Descripción\s+(.+)/) || '';
+  const matchDescription =
+    (fields[1] && fields[1].match(/^Descripción\s+(.+)/)) || "";
   const description = matchDescription ? matchDescription[1] : "";
 
-  const matchAmount = fields[2] && fields[2].match(
-    /^Importe\s+(-?\d{1,3}(?:\.\d{3})*(?:,\d{1,2})?)/
-  ) || '';
+  const matchAmount =
+    (fields[2] &&
+      fields[2].match(/^Importe\s+(-?\d{1,3}(?:\.\d{3})*(?:,\d{1,2})?)/)) ||
+    "";
   const amount = matchAmount
     ? parseFloat(matchAmount[1].replace(".", "").replace(",", "."))
     : NaN;
 
-  const matchCurrency = fields[3] && fields[3].match(/^Divisa\s+(.+)/) || '';
+  const matchCurrency = (fields[3] && fields[3].match(/^Divisa\s+(.+)/)) || "";
   const currency = matchCurrency ? matchCurrency[1] : "";
 
-  const matchDate = fields[4] && fields[4].match(
-    /^Fecha del movimiento\s+(\d{2}\/\d{2}\/\d{4})/
-  ) || '';
+  const matchDate =
+    (fields[4] &&
+      fields[4].match(/^Fecha del movimiento\s+(\d{2}\/\d{2}\/\d{4})/)) ||
+    "";
   const date = matchDate ? formatDate(matchDate[1]) : null;
 
-  const matchValueDate = fields[5] && fields[5].match(
-    /^Fecha valor\s+(\d{2}\/\d{2}\/\d{4})/
-  ) || '';
+  const matchValueDate =
+    (fields[5] && fields[5].match(/^Fecha valor\s+(\d{2}\/\d{2}\/\d{4})/)) ||
+    "";
   const valueDate = matchValueDate ? formatDate(matchValueDate[1]) : null;
 
-  const matchAccount = fields[6] && fields[6].match(/^Cuenta cargo\/abono\s+(.+)/) || '';
+  const matchAccount =
+    (fields[6] && fields[6].match(/^Cuenta cargo\/abono\s+(.+)/)) || "";
   const account = matchAccount ? matchAccount[1] : "";
 
-  const matchAccountHolder = fields[7] && fields[7].match(/^Titular de la cuenta\s+(.+)/) || '';
+  const matchAccountHolder =
+    (fields[7] && fields[7].match(/^Titular de la cuenta\s+(.+)/)) || "";
   const accountHolder = matchAccountHolder ? matchAccountHolder[1] : "";
 
-  const matchNotes = fields[8] && fields[8].match(/^Observaciones\s+(.+)/) || '';
+  const matchNotes =
+    (fields[8] && fields[8].match(/^Observaciones\s+(.+)/)) || "";
   const notes = matchNotes ? matchNotes[1] : "";
 
   if (
@@ -81,59 +87,59 @@ function parseTransactionInfo(text: string): any | null {
 
   return {
     type: type,
-    desc: description,
+    Name: description,
     amount: Math.abs(amount),
     currency: currency,
     Date: valueDate,
     //valueDate: valueDate,
     account: account,
-    accountHolder: accountHolder,
+    Account_holder: accountHolder,
     notes: notes,
   };
 }
 
-const NewExpense = ({ loading, onCreate = (params) => { } }) => {
+const NewExpense = ({ loading, onCreate = (params) => {} }) => {
   const [text, setText] = useState("");
-  const [transaction, setTransaction] = useState<TransactionInfo>({})
-  
+  const [transaction, setTransaction] = useState<TransactionInfo>({});
+
   const handleChange = (e: any) => {
     setText(e.target.value);
   };
 
   useEffect(() => {
-    const transaction = parseTransactionInfo(text)
-    if (transaction) setTransaction(transaction)
+    const transaction = parseTransactionInfo(text);
+    if (transaction) setTransaction(transaction);
   }, [text]);
-  
-  const fields = Object.keys(transaction)
-  
+
+  const fields = Object.keys(transaction);
+
   const containerStyles = {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'space-between',
-    marginBottom: '20px',
-    fontSize: '0.8rem'
-  }
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    marginBottom: "20px",
+    fontSize: "0.8rem",
+  };
   const textAreaStyles = {
     border: "1px solid gray",
     height: "250px",
     width: "50%",
-    fontSize: '0.8rem',
-    paddingLeft: '20px'
-  }
-  
+    fontSize: "0.8rem",
+    paddingLeft: "20px",
+  };
+
   const resultPaneStyle = {
-    paddingLeft: '20px',
-    display: 'flex',
-    flexDirection: 'column'
-  }
+    paddingLeft: "20px",
+    display: "flex",
+    flexDirection: "column",
+  };
 
   const fieldStyles = {
-    borderBottom: '1px solid gray',
-    marginBottom: '5px',
-    display: 'flex',
-    justifyContent: 'space-between'
-  }
+    borderBottom: "1px solid gray",
+    marginBottom: "5px",
+    display: "flex",
+    justifyContent: "space-between",
+  };
 
   const buttonStyles =
     "bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer";
@@ -150,23 +156,31 @@ const NewExpense = ({ loading, onCreate = (params) => { } }) => {
 
   return (
     <div style={containerStyles}>
-      
-        <textarea
-          style={textAreaStyles}
-          value={text}
-          onChange={handleChange}
-        ></textarea>
-      
+      <textarea
+        style={textAreaStyles}
+        value={text}
+        onChange={handleChange}
+      ></textarea>
+
       <div style={resultPaneStyle}>
-        {!!fields.length && <>{
-          fields.map(field => {
-            return <div style={fieldStyles} key={field}><span>{field}:</span><span>{transaction[field].toString()}</span> </div>
-          })
-          
-        }
-          <button className={buttonStyles} onClick={() => onCreate({ body: transaction })}>
-            Confirmar {loading && <Spinner /> }</button>
-        </>}
+        {!!fields.length && (
+          <>
+            {fields.map((field) => {
+              return (
+                <div style={fieldStyles} key={field}>
+                  <span>{field}:</span>
+                  <span>{transaction[field].toString()}</span>{" "}
+                </div>
+              );
+            })}
+            <button
+              className={buttonStyles}
+              onClick={() => onCreate({ body: transaction })}
+            >
+              Confirmar {loading && <Spinner />}
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
