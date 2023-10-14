07f50fdb09f8fc5c5b4e5d32d4ba360f693cf495
christianlb <christian.aguero.1983@gmail.com>
2023-10-14 13:54:49 +0200

    adding node cron

--- cron.js
+++ cron.js
@@ -1,2 +1,7 @@
-const date = new Date();
-console.log(`Cron job executed at: ${date.toISOString()}`);
+// Importa las dependencias necesarias
+const cron = require('node-cron');
+
+// Define y inicia la tarea cron
+cron.schedule('*/1 * * * *', () => {
+  console.log('Cron job executed at:', new Date().toISOString());
+});
