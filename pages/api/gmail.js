d999d7093dc61fd745bfb980de1210c3263053d8
christian aguerom <christian.aguero.1983@gmail.com>
2023-11-03 20:41:42 +0100

    logging refreshtoken

--- pages/api/gmail.js
+++ pages/api/gmail.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     refresh_token: session.refreshToken,
   });
 
-  //console.log(session.refreshToken);
+  console.log(session.refreshToken);
 
   const gmail = google.gmail({ version: "v1", auth: oauth2Client });
 
