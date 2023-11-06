302913126cb37f7393e6dec5393300897c640c72
christianlb <christian.aguero.1983@gmail.com>
2023-11-06 16:45:50 +0100

    resume changes

--- pages/resume.js
+++ pages/resume.js
@@ -1,36 +1,20 @@
 import styles from "../styles/Resume.module.css";
 
-const ResumePage = () => {
+const CV = () => {
   return (
-    <div className={styles.resume}>
-      <div className={styles.header}>
-        <h1 className={'h1'}>Dani Schwaiger</h1>
-        <h2 className={'h2'} >Web Developer</h2>
+    <div className={styles.a4}>
+      {/* Contenido del CV aquí */}
+      <div className={styles.column} id="left-column">
+        {/* Información personal, habilidades, idiomas */}
       </div>
-      <div className={styles.contactInfo}>
-        <p>123-456-7890</p>
-        <p>hello@reallygr eatsite.com</p>
-        <p>123 Anywhere St., Any City</p>
-        <p>reallygreatsite.com</p>
+      <div className={styles.column} id="right-column">
+        {/* Historial laboral, educación */}
       </div>
-      <div className={styles.profile}>
-        <h3 className={'h3'}>Profile</h3>
-        <p>{/* Profile info goes here */}</p>
-      </div>
-      <div className={styles.skills}>
-        <h3 className={'h3'}>Skills</h3>
-        <ul>{/* Skill list goes here */}</ul>
-      </div>
-      <div className={styles.experience}>
-        <h3 className={'h3'}>Experience</h3>
-        {/* Experience items go here */}
-      </div>
-      <div className={styles.education}>
-        <h3 className={'h3'}>Education</h3>
-        {/* Education items go here */}
-      </div>
+      <button onClick={() => window.print()} className={styles.printButton}>
+        Imprimir CV
+      </button>
     </div>
   );
 };
 
-export default ResumePage;
+export default CV;
