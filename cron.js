868ad43f4ab8cd0f31b2da0100c54933aac7b400
christianlb <christian.aguero.1983@gmail.com>
2023-12-14 00:54:52 +0100

    fix cron

--- cron.js
+++ cron.js
@@ -1,10 +1,17 @@
 // Importa las dependencias necesarias
-const cron = require("node-cron");
-const checkGmail = require("./checkMail");
+const cron = require('node-cron');
+const axios = require('axios');
 
-// Programa la tarea para que se ejecute cada minuto (ajusta la expresión cron según tus necesidades)
-cron.schedule("*/1 * * * *", () => {
-  checkGmail().catch(error => {
-    console.error("Error:", error);
-  });
+// URL de tu endpoint
+const endpointURL = `${process.env.API_URL}/checkMail`;
+
+// Programa la tarea para que se ejecute cada minuto
+cron.schedule('*/1 * * * *', () => {
+  axios.get(endpointURL)
+    .then(response => {
+      console.log('Respuesta del endpoint:', response.data);
+    })
+    .catch(error => {
+      console.error('Error al realizar la solicitud GET:', error);
+    });
 });
